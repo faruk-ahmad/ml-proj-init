6e6992bda64d132c98a50aec9c5c95f63067d9a1
faruk-ahmad <faruk.csebrur@gmail.com>
2020-05-17 02:23:59 +0900

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as rf:
+    long_description = rf.read()
+
+with open('requirements.txt', 'r') as f:
+    install_requires = [requirement for requirement in f.read().split('\n') if requirement]
+
+
+setuptools.setup(
+    name="ml_proj_init",
+    version="1.0.0",
+    author="faruk-ahmad",
+    author_email="faruk.csebrur@gmail.com",
+    description="Machine learning and Deep learning project structure initializer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/faruk-ahmad/ml-proj-init",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'ml-proj-init = ml_proj_init.__main__:main',
+        ]
+    },
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
