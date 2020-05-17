b04487ed87962345eeb9303b4e4b146676b54830
faruk-ahmad <faruk.csebrur@gmail.com>
2020-05-17 17:37:59 +0900

    created validation.py for Validation class

---
+++ ml_proj_init/validation.py
@@ -0,0 +1,11 @@
+"""
+A module for validatinig different parameters given by user
+"""
+
+class Validation():
+    """
+    A class holding required methods for validating user inputs
+    """
+
+    def __init__(self):
+        pass
