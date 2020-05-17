957803df70c0715d3ced36f17a8265134e558858
faruk-ahmad <faruk.csebrur@gmail.com>
2020-05-17 17:37:43 +0900

    created utils.py for Utility class

---
+++ ml_proj_init/utils.py
@@ -0,0 +1,12 @@
+"""
+A utility module for performing some utility task to initialize ml or dl projects
+"""
+
+
+class Utility():
+    """
+    A class that holds necessary methods for some utility tasks
+    """
+
+    def __init__(self):
+        pass
