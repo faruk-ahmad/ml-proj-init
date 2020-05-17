5295a5abdd702f974cf4e29b7a8f7f8fbb54984e
faruk-ahmad <faruk.csebrur@gmail.com>
2020-05-18 02:32:03 +0900

    added util method for user consent and deleting existing path

--- ml_proj_init/utils.py
+++ ml_proj_init/utils.py
@@ -2,6 +2,9 @@
 A utility module for performing some utility task to initialize ml or dl projects
 """
 
+import os
+import sys
+import shutil
 
 class Utility():
     """
@@ -10,3 +13,27 @@ class Utility():
 
     def __init__(self):
         pass
+
+
+    def get_user_consent(self):
+        """
+        Method to prompt for deletion and get user consent
+        """
+        print("Path already exists, do you want to delete it?")
+
+        consent = input("Y/N: ").lower()
+
+        return consent
+
+    def delete_existing_path(self, proj_path, proj_name):
+        """
+        Method to try to delete existing path based on user consent
+        """
+        try:
+            proj_full_path = os.path.join(proj_path, proj_name)
+            shutil.rmtree(proj_full_path)
+        except Exception as e:
+            print("Path not writable, can not delete directory.")
+            return False
+        else:
+            return True
