b936e4006e77ef5c3b67ff1e510c13bbe920d4ca
faruk-ahmad <faruk.csebrur@gmail.com>
2020-05-18 02:31:15 +0900

    added new config parameter

--- ml_proj_init/config.py
+++ ml_proj_init/config.py
@@ -9,5 +9,6 @@ config = {
     "run_name_choices": ["c", "a"],
     "proj_name_first_char": string.ascii_lowercase,
     "proj_name_allowed_char": string.ascii_lowercase + string.digits + "_",
-    "proj_name_length": 80
+    "proj_name_length": 80,
+    "valid_proj_type": ["ml", "dl"]
 }
