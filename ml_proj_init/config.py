913b0a55b6851588d232605ea31f42bfa504f029
faruk-ahmad <faruk.csebrur@gmail.com>
2020-05-18 23:51:39 +0900

    updated path of data dir to get absolute path of template directory

--- ml_proj_init/config.py
+++ ml_proj_init/config.py
@@ -5,10 +5,20 @@ A module for defining the config parameters globally
 import os
 import string
 
+current_abs_path = os.path.abspath(os.path.dirname(__file__))
+print(current_abs_path)
+
 config = {
     "run_name_choices": ["c", "a"],
     "proj_name_first_char": string.ascii_lowercase,
-    "proj_name_allowed_char": string.ascii_lowercase + string.digits + "_",
+    "proj_name_allowed_char": string.ascii_lowercase + string.digits + "_" + "-",
     "proj_name_length": 80,
-    "valid_proj_type": ["ml", "dl"]
+    "valid_proj_type": ["ml", "dl"],
+    "ml_template_path": os.path.join(current_abs_path, "data/ml"),
+    "dl_template_path": os.path.join(current_abs_path, "data/dl")
 }
+
+
+if __name__ == "__main__":
+    print(config["proj_name_first_char"])
+    print(config["proj_name_allowed_char"])
